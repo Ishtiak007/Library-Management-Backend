5f1abacb8e5a21549b13fb9136fd36f731385a89
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-11-07 23:19:06 +0600

    To get specific data from allbooks collection I did post operation from client side and did get operation from server side. And also done put operation for update a book information from client side.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // 4 category related api collection
     const categoriesCollection = client.db('libraryManagementSystem').collection('categories');
@@ -45,7 +45,6 @@ async function run() {
     // all books related API
     app.post('/allBooks',async(req,res)=>{
         const newBooks = req.body;
-        console.log(newBooks)
         const result = await booksCollection.insertOne(newBooks);
         res.send(result);
     });
@@ -54,12 +53,35 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     });
+    app.get('/allBooks/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await booksCollection.findOne(query);
+        res.send(result);
+    });
+    app.put('/allBooks/:id',async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id : new ObjectId(id)}
+        const options = {upsert : true};
+        const updatedBook = req.body;
+        const newBook ={
+            $set:{
+                image : updatedBook.image,
+                bookName : updatedBook.bookName,
+                author : updatedBook.author,
+                category : updatedBook.category,
+                rating : updatedBook.rating,
+            }
+        }
+        const result = await booksCollection.updateOne(filter,newBook, options)
+        res.send(result);
+    });
 
 
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB! Alhumdulilah!");
   } finally {
     // Ensures that the client will close when you finish/error
