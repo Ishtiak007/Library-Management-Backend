dc94d37fdb71653cb2c2c91400838c1106520bf2
Ishtiak Ahmed <ishtiak.sparrow98@gmail.com>
2023-11-08 20:40:16 +0600

    Implemented Json Web Token with its all reqirements, ready to assignment suubmit

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin : [
+        // 'http://localhost:5173'
+        'https://library-management-syste-e5864.web.app',
+        'https://library-management-syste-e5864.firebaseapp.com'
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
-//libraryManagementSystem
-//categories
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1bw0ez.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,6 +33,32 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+// middlewares
+const logger =(req,res,next)=>{
+    console.log('log : info', req.method, req.url);
+    next();
+  }
+  const verifyToken  = (req,res,next)=>{
+    const token = req?.cookies?.token;
+    // console.log('token in the middleware',token);
+    if(!token){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+      if(err){
+        return res.status(401).send({message : 'unauthorize access'});
+      }
+      req.user = decoded;
+      next();
+    })   
+}
+
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +68,35 @@ async function run() {
     const categoriesCollection = client.db('libraryManagementSystem').collection('categories');
     const booksCollection = client.db('libraryManagementSystem').collection('allBooks');
     const bookBorrowerCollection = client.db('libraryManagementSystem').collection('bookBorrower');
+
+
+
+
+
+
+    // JWT related api
+    app.post('/jwt', logger , async(req,res)=>{
+        const user = req.body;
+        console.log('user for token', user);
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+        res
+        .cookie('token',token,{
+          httpOnly:true,
+          secure:process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none':'strict'
+        })
+        .send({success : true});  
+      });
+      app.post('/logout',async(req,res)=>{
+        const user = req.body;
+        console.log('logging out user',user)
+        res.clearCookie('token',{maxAge:0}).send({success: true})
+      })
+
+
+
+
+
 
 
     //4 category related api
@@ -84,7 +148,12 @@ async function run() {
         const result = await bookBorrowerCollection.insertOne(borrower);
         res.send(result);
     });
-    app.get('/bookBorrower',async (req,res)=>{
+    app.get('/bookBorrower', logger , verifyToken, async (req,res)=>{
+        console.log(req.query.email)
+        console.log('token owner info',req.user);
+        if(req.user.email !== req.query.email){
+          return res.status(403).send({message :'forbidden access'})
+        }
           let query = {}
           if(req.query?.email){
               query = {email : req.query.email}
